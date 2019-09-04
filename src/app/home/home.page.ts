55c9ec6fc2046587dc793320524a94c27ff161c0
KevinNadeau <kevin.nadeau@hotmail.fr>
2019-09-04 03:24:45 +0200

    application ionic watchome

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environement } from '../../models/environements';
+
+
 
 @Component({
   selector: 'app-home',
@@ -6,7 +10,38 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  nom: string;
+  description: string;
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+    this.loadArticles();
+}
+
+  loadArticles(): void {
+    const url = `${environement.api_url}/Articles`;
+    console.log('url', url);
+    this.http.get(url)
+    .subscribe(articles => console.log ('articles', articles));
+  }
+
+  insertArticle(): void {
+    const url = `${environement.api_url}/Articles`;
+    this.http.post(url, { nom: this.nom, description: this.description })
+      .subscribe(results => console.log('results', results));
+  }
+
+  updateArticle(): void {
+    const id = '5d6ede6c5c857640706ac8a4';
+    const url =  `${environement.api_url}/Articles/${id}`;
+    this.http.patch(url, {nom: 'Alarme intelligente'})
+      .subscribe(result => console.log('result', result));
+  }
+
+  removeArticle(): void {
+    const id = '5d6f079b3eb1ad36683cd2e4';
+    const url =  `${environement.api_url}/Articles/${id}`;
+    this.http.delete(url)
+      .subscribe(result => console.log('result', result));
+  }
 
 }
